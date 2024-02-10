2ded8ebb4979f8f3a050f7de669d02e539555e00
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-02-10 19:43:20 +0100

    DONE
    never use postman alone this made me lighheaded

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const http = require('http');
 const fs = require("fs");
+const { url } = require('inspector');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  // Your code here
+  
+  if (req.url.startsWith('/static')) {
+    let filePath = "." + req.url.replace("/static", '/assets');
+    let fileExtension = path.extname(filePath).slice(1);
+    
+    switch (fileExtension) {
+      case 'css':
+        res.setHeader('Content-Type', 'text/css');
+        res.body = fs.readFileSync(filePath, 'utf-8');
+        break;
+      case 'jpg':
+      case 'jpeg':
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.body = fs.readFileSync(filePath);
+        break;
+    }
+    res.statusCode = 200;
+    return res.end(res.body);
+  }
+  let indexHtml = fs.readFileSync('./index.html');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.body = indexHtml;
+  res.end(res.body)
 });
 
 const port = 5000;
